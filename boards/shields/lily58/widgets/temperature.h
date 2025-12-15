bd634325a525e7f01c610726c7fd1724907c98bb
cantruchd <truc294@gmail.com>
2025-12-15 17:40:31 +0800

    Create temperature.h for widget implementation
    Add header file for temperature widget with initialization function.

--- boards/shields/lily58/widgets/temperature.h
+++ boards/shields/lily58/widgets/temperature.h
@@ -1,1 +1,12 @@
+// File: config/widgets/temperature.h
+// Header file cho temperature widget
 
+#pragma once
+
+#include <lvgl.h>
+
+struct zmk_widget_temperature {
+    lv_obj_t *obj;
+};
+
+int zmk_widget_temperature_init(struct zmk_widget_temperature *widget, lv_obj_t *parent);
