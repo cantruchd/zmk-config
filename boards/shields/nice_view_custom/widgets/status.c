d7206ab22281971f7b1ed6b1b89ebdefaafa7a91
cantruchd <truc294@gmail.com>
2025-12-29 17:10:42 +0800

    ok

--- boards/shields/nice_view_custom/widgets/status.c
+++ boards/shields/nice_view_custom/widgets/status.c
@@ -148,7 +148,7 @@ static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_st
     init_line_dsc(&line_dsc, LVGL_FOREGROUND, 1);
 
     lv_draw_label_dsc_t label_dsc_temp_unit;
-    init_label_dsc(&label_dsc_temp_unit, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_RIGHT);
+    init_label_dsc(&label_dsc_temp_unit, LVGL_FOREGROUND, &lv_font_montserrat_14, LV_TEXT_ALIGN_RIGHT);
 
 
     // Fill background
@@ -236,7 +236,7 @@ static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_st
     // max_width=64: chiều rộng khung - 2px padding
     lv_canvas_draw_text(canvas, 0, 37, 68, &label_dsc_temp, temp_text);
 
-    lv_canvas_draw_text(canvas, 0, 47, 68, &label_dsc_temp_unit, "°C");
+    lv_canvas_draw_text(canvas, 0, 41, 68, &label_dsc_temp_unit, "°C");
 
     // Rotate canvas
     rotate_canvas(canvas, cbuf);
