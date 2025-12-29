61c0e201c0995ae74902710dc3b11b30f87396cb
cantruchd <truc294@gmail.com>
2025-12-29 16:08:39 +0800

    Update status.c

--- boards/shields/nice_view_custom/widgets/status.c
+++ boards/shields/nice_view_custom/widgets/status.c
@@ -111,7 +111,7 @@ static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_st
 
     char wpm_text[6] = {};
     snprintf(wpm_text, sizeof(wpm_text), "%d", max);
-    lv_canvas_draw_text(canvas, 18, 23, 48, &label_dsc_wpm, wpm_text);
+    lv_canvas_draw_text(canvas, 18, 13, 48, &label_dsc_wpm, wpm_text);
     
     int range = max - min;
     if (range == 0) {
