6a20724bba60a2ca425e799cf266f61d5a6a08fc
cantruchd <truc294@gmail.com>
2026-01-15 21:42:42 +0800

    Update battery_monitor.c

--- boards/shields/battery_monitor/battery_monitor.c
+++ boards/shields/battery_monitor/battery_monitor.c
@@ -1380,9 +1380,10 @@ static ssize_t read_bond_management(struct bt_conn *conn, const struct bt_gatt_a
         memcpy(bond_data, &bond_list[i].addr, 7);
         memcpy(bond_data + 7, bond_list[i].alias, BOND_ALIAS_MAX_LEN);
         bond_data[39] = bond_list[i].is_connected ? 0x01 : 0x00;
+        LOG_INF("Bond list read: %d bonds WITH ALIAS %s", bond_count, bond_list[i].alias);
     }
     
-    LOG_INF("Bond list read: %d bonds WITH ALIAS %s", bond_count, bond_list[0].alias);
+    
     return bt_gatt_attr_read(conn, attr, buf, len, offset, data, 1 + (bond_count * 40));
 }
 
