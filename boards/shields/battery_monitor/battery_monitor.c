88102136fc5a08864661e2ca8ca4c960a95b839e
cantruchd <truc294@gmail.com>
2026-01-19 10:55:03 +0800

    Update battery_monitor.c

--- boards/shields/battery_monitor/battery_monitor.c
+++ boards/shields/battery_monitor/battery_monitor.c
@@ -127,7 +127,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
     BT_UUID_DECLARE_128(BT_UUID_BOND_MANAGEMENT_VAL)
 
 
-/ ⭐ CRITICAL: Enable event manager to dispatch to this listener
+// ⭐ CRITICAL: Enable event manager to dispatch to this listener
 #define ZMK_EV_EVENT_TYPE(ev) zmk_battery_state_changed
 #define ZMK_EV_EVENT_LIST_ITEM(ev) ZMK_LISTENER_CB(battery_monitor, ev)
 
