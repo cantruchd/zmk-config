b3113089f9a26d15a3e2f827000d9e3a3d98aff3
cantruchd <truc294@gmail.com>
2026-01-22 21:17:41 +0800

    Update peripheral_status.c

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -386,7 +386,7 @@ static void draw_middle(lv_obj_t *canvas, lv_color_t cbuf[]) {
     init_label_dsc(&label_dsc_v, LVGL_FOREGROUND, &lv_font_montserrat_26, LV_TEXT_ALIGN_LEFT);
     init_label_dsc(&label_dsc_v_label, LVGL_FOREGROUND, &lv_font_montserrat_24, LV_TEXT_ALIGN_RIGHT);
 
-    init_label_dsc(&label_dsc_v_label_uptime, LVGL_FOREGROUND, &lv_font_montserrat_24, LV_TEXT_ALIGN_MIDDLE);
+    init_label_dsc(&label_dsc_v_label_uptime, LVGL_FOREGROUND, &lv_font_montserrat_26, LV_TEXT_ALIGN_MIDDLE);
 
     lv_canvas_draw_rect(canvas, 0, 0, MIDDLE_WIDTH, MIDDLE_HEIGHT, &rect_black_dsc);
 
@@ -424,8 +424,8 @@ static void draw_middle(lv_obj_t *canvas, lv_color_t cbuf[]) {
     snprintf(uptime_hourmin, sizeof(uptime_hourmin), "%02d:%02d", uptime.hours, uptime.minutes);
 
     LOG_WRN("Drawing uptime: %s %s", uptime_day, uptime_hourmin);
-    lv_canvas_draw_text(canvas, 0, 20, MIDDLE_WIDTH, &label_dsc_v_label_uptime, uptime_day);
-    lv_canvas_draw_text(canvas, 0, 40, MIDDLE_WIDTH, &label_dsc_v_label_uptime, uptime_hourmin);
+    lv_canvas_draw_text(canvas, 0, 22, MIDDLE_WIDTH, &label_dsc_v_label_uptime, uptime_day);
+    lv_canvas_draw_text(canvas, 0, 44, MIDDLE_WIDTH, &label_dsc_v_label_uptime, uptime_hourmin);
 
     rotate_canvas(canvas, cbuf);
 
