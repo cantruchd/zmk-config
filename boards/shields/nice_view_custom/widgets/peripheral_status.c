cdc098ef10cb9b2399df45b095efd6567405b8dd
cantruchd <truc294@gmail.com>
2026-01-22 21:18:55 +0800

    Update peripheral_status.c

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -382,7 +382,7 @@ static void draw_middle(lv_obj_t *canvas, lv_color_t cbuf[]) {
     lv_draw_rect_dsc_t rect_black_dsc;
     init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
     
-    lv_draw_label_dsc_t label_dsc_v, label_dsc_v_label;
+    lv_draw_label_dsc_t label_dsc_v, label_dsc_v_label, label_dsc_v_label_uptime;
     init_label_dsc(&label_dsc_v, LVGL_FOREGROUND, &lv_font_montserrat_26, LV_TEXT_ALIGN_LEFT);
     init_label_dsc(&label_dsc_v_label, LVGL_FOREGROUND, &lv_font_montserrat_24, LV_TEXT_ALIGN_RIGHT);
 
