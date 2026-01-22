44c37145c30958d694307a9a4b92de192641cd21
cantruchd <truc294@gmail.com>
2026-01-22 21:50:10 +0800

    ok

--- boards/shields/nice_view_custom/behaviors/behavior_uptime_reset.c
+++ boards/shields/nice_view_custom/behaviors/behavior_uptime_reset.c
@@ -1,24 +1,25 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ */
+
 #define DT_DRV_COMPAT zmk_behavior_uptime_reset
 
 #include <zephyr/device.h>
 #include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
-#include <zmk/behavior.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-// Import hàm reset từ peripheral_status.c
+#include <zmk/behavior.h>
+
+// Extern function từ peripheral_status.c
 extern int uptime_reset(void);
 
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                       struct zmk_behavior_binding_event event) {
-    LOG_INF("Uptime reset triggered!");
-    int rc = uptime_reset();
-    if (rc == 0) {
-        LOG_INF("Uptime reset successful");
-    } else {
-        LOG_ERR("Uptime reset failed: %d", rc);
-    }
+    LOG_WRN("Uptime reset triggered!");
+    uptime_reset();
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
@@ -32,6 +33,10 @@ static const struct behavior_driver_api behavior_uptime_reset_driver_api = {
     .binding_released = on_keymap_binding_released,
 };
 
-BEHAVIOR_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL, APPLICATION,
-                        CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
-                        &behavior_uptime_reset_driver_api);
+static int behavior_uptime_reset_init(const struct device *dev) {
+    return 0;
+}
+
+DEVICE_DT_INST_DEFINE(0, behavior_uptime_reset_init, NULL, NULL, NULL,
+                      APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+                      &behavior_uptime_reset_driver_api);
